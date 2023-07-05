cb924abaf40c87d31c8a1286f673a8d5b5489263
rrenatars <renat.arslanov2015@yandex.ru>
2023-07-05 16:20:33 +0300

    уменьшение маны после вытаскивания карты

--- static/js/arena.js
+++ static/js/arena.js
@@ -1,7 +1,17 @@
 const cardsListElement = document.querySelector(`.main__cards`);
+const cardsElements = document.querySelectorAll('.cards__card')
 const fieldListElement = document.querySelector('.main__field');
+const manaElement = document.getElementById('MyMana');
+let mana = manaElement.textContent;
 
 cardsListElement.addEventListener(`dragstart`, (evt) => {
+    const selectedCardMana = evt.target.querySelector('.card__mana').textContent;
+    tempManaAfterDrag = (parseInt(mana) - parseInt(selectedCardMana));
+    if (tempManaAfterDrag <= 0) {
+        evt.preventDefault();
+        alert("Недостаточно маны");
+        return;
+    }
     evt.target.classList.add(`selected`);
 });
 
@@ -28,13 +38,17 @@ fieldListElement.addEventListener(`dragover`, (evt) => {
 
     const nextElement = currentElement.nextElementSibling;
 
-
     if (nextElement !== null && activeElement !== null) {
+
         fieldListElement.insertBefore(activeElement, nextElement);
         fieldListElement.removeChild(currentElement);
+        manaElement.textContent = tempManaAfterDrag + "/10";
+        mana = tempManaAfterDrag;
     } else if (activeElement !== null) {
         fieldListElement.appendChild(activeElement);
         fieldListElement.removeChild(currentElement);
+        manaElement.textContent = tempManaAfterDrag + "/10";
+        mana = tempManaAfterDrag;
     }
-
 });
+
