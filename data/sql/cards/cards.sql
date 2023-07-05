1c95b26b18f7fc547572e036c19c94bb8888a0ea
Ilsaf221 <124866755+Ilsaf221@users.noreply.github.com>
2023-07-05 17:29:05 +0300

    Add files via upload

---
+++ data/sql/cards/cards.sql
@@ -0,0 +1,57 @@
+INSERT INTO
+    card
+    (
+        name,
+        mana,
+        healthpoint,
+        attack
+    )
+VALUES
+(
+    'Golem',
+    '4',
+    '5',
+    '5'
+);
+
+INSERT INTO
+    card
+    (
+        name,
+        mana
+    )
+VALUES
+(
+    'Waterball',
+    '4'
+);
+
+INSERT INTO
+    card
+(
+    name,
+    mana
+)
+VALUES
+(
+    'Fireball',
+    '4'
+);
+
+INSERT INTO
+    card
+(
+    name,
+    mana,
+    healthpoint,
+    attack,
+    portrait
+)
+VALUES
+(
+    'Boulderfist Ogre',
+    '6',
+    '7',
+    '6',
+    '/static/images/cards-in-hand/Boulderfist Ogre.png'
+);
