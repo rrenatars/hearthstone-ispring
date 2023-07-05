63e35929ad1694d201823209ad24e2b94e3f623a
Ilsaf221 <124866755+Ilsaf221@users.noreply.github.com>
2023-07-05 10:31:07 +0300

    Рисовка поле (фон, карточки, мана)

--- index.js
+++
@@ -1,8 +0,0 @@
-const collisionbox = document.getElementById("collisionbox");
-
-function createManaCrystal() {
-    const manacontainer = document.getElementById("manacontainer");
-    const manacrystal = document.createElement('div');
-    manacrystal.classList.add("manabox");
-    manacontainer.appendChild(manacrystal);
-  }
