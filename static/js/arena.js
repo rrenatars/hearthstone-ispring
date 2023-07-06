3f06d24762344b134a1fab5781d7d5b89c571a86
rrenatars <renat.arslanov2015@yandex.ru>
2023-07-06 10:01:52 +0300

    merge ренат и ильсаф

--- static/js/arena.js
+++ static/js/arena.js
@@ -1,14 +1,35 @@
-const cardsListElement = document.querySelector(`.main__cards`);
-const fieldListElement = document.querySelector('.main__field');
 const endTurnButton = document.getElementById('endturn')
-emptyFieldHidden = fieldListElement.querySelector('.field__empty_hidden');
+const cardsListElement = document.querySelector(`.background__cards`);
+const cardsElements = document.querySelectorAll('.cards__card')
+const fieldListElement = document.querySelector('.background__field');
+const manaElement = document.getElementById('MyMana');
+let mana = parseInt(manaElement.textContent);
+const manabar = document.getElementById('Manabar');
+
+function manabarFilling() {
+    const arr = [];
+
+    for (let i = 1; i <= mana; i++) {
+        const manaCrystalImage = document.createElement('img');
+        manaCrystalImage.src = '../static/images/field/mana.png';
+        manaCrystalImage.setAttribute('class', 'manabar__crystall');
+        arr.push(manaCrystalImage);
+    }
+
+    manabar.replaceChildren(...arr);
+}
+
+manabarFilling();
 
 cardsListElement.addEventListener(`dragstart`, (evt) => {
+    const selectedCardMana = evt.target.querySelector('.card__mana').textContent;
+    tempManaAfterDrag = (parseInt(mana) - parseInt(selectedCardMana));
+    if (tempManaAfterDrag < 0) {
+        evt.preventDefault();
+        alert("Недостаточно маны");
+        return;
+    }
     evt.target.classList.add(`selected`);
-    selectedCard = cardsListElement.querySelector('.selected');
-    divCard = selectedCard.closest('div');
-    // emptyFieldHidden.removeAttribute('class');
-    // emptyFieldHidden.setAttribute('class', 'field__empty');
 });
 
 fieldListElement.addEventListener(`dragend`, (evt) => {
@@ -34,17 +55,20 @@ fieldListElement.addEventListener(`dragover`, (evt) => {
 
     const nextElement = currentElement.nextElementSibling;
 
-
     if (nextElement !== null && activeElement !== null) {
+
         fieldListElement.insertBefore(activeElement, nextElement);
         fieldListElement.removeChild(currentElement);
-        cardsListElement.removeChild(divCard);
+        manaElement.textContent = tempManaAfterDrag + "/10";
+        mana = tempManaAfterDrag;
+        manabarFilling();
     } else if (activeElement !== null) {
         fieldListElement.appendChild(activeElement);
         fieldListElement.removeChild(currentElement);
-        cardsListElement.removeChild(divCard);
+        manaElement.textContent = tempManaAfterDrag + "/10";
+        mana = tempManaAfterDrag;
+        manabarFilling();
     }
-
 });
 
 endTurnButton.addEventListener(
