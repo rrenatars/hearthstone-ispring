e72f1cd43246e26cac8b22c764512cbdcc5e300d
rrenatars <renat.arslanov2015@yandex.ru>
2023-07-07 14:23:57 +0300

    реализовал адекватное перетаскивание карт

--- static/js/arena.js
+++ static/js/arena.js
@@ -1,6 +1,3 @@
-const cardsListElement = document.querySelector(`.background__cards`);
-const fieldListElement = document.querySelector('.background__field');
-const fieldEmpty = document.querySelector('.field__empty');
 const manaElement = document.getElementById('MyMana');
 let mana = parseInt(manaElement.textContent);
 const manabar = document.getElementById('Manabar');
@@ -15,51 +12,16 @@ function manabarFilling() {
         arr.push(manaCrystalImage);
     }
 
+    manaElement.textContent = mana + '/10';
     manabar.replaceChildren(...arr);
 }
 
 manabarFilling();
 
-var cards = document.getElementsByClassName('cards__card');
+const field = document.querySelector('.background__field');
+const hand = document.querySelector('.background__cards');
 
-for (var i = 0; i < cards.length; i++) {
-    (function(card) {
-        card.onmousedown = function(e) {
-            var coords = getCoords(card);
-            var shiftX = e.pageX - coords.left;
-            var shiftY = e.pageY - coords.top;
-
-            card.style.position = 'absolute';
-            document.body.appendChild(card);
-            moveAt(e);
-
-            card.style.zIndex = 1000; // над другими элементами
-
-            function moveAt(e) {
-                card.style.left = e.pageX - shiftX + 'px';
-                card.style.top = e.pageY - shiftY + 'px';
-            }
-
-            document.onmousemove = function(e) {
-                moveAt(e);
-            };
-
-            card.onmouseup = function() {
-                document.onmousemove = null;
-                card.onmouseup = null;
-            };
-        };
-
-        card.ondragstart = function() {
-            return false;
-        };
-
-
-
-    })(cards[i]);
-}
-
-function getCoords(elem) {   // кроме IE8-
+function getCoords(elem) {
     var box = elem.getBoundingClientRect();
     return {
         top: box.top + scrollY,
@@ -67,73 +29,68 @@ function getCoords(elem) {   // кроме IE8-
     };
 }
 
+const handLimits = 420;
+let cards = document.getElementsByClassName('cards__card');
 
+for (var i = 0; i < cards.length; i++) {
+    (function (card) {
+        card.onmousedown = function (e) {
+            if (card.classList.contains('cards__card')) {
+                let manaSelectedCard = parseInt(card.querySelector('.card__mana').textContent);
+                if ((mana - manaSelectedCard) < 0) {
+                    alert("Недостаточно маны");
+                    return;
+                } else {
+                    var coords = getCoords(card);
+                    var shiftX = e.pageX - coords.left;
+                    var shiftY = e.pageY - coords.top;
 
-//
-// cardsListElement.addEventListener(`dragstart`, (evt) => {
-//     const selectedCardMana = evt.target.querySelector('.card__mana').textContent;
-//     tempManaAfterDrag = (parseInt(mana) - parseInt(selectedCardMana));
-//     if (tempManaAfterDrag < 0) {
-//         evt.preventDefault();
-//         alert("Недостаточно маны");
-//         return;
-//     }
-//     evt.target.classList.add(`selected`);
-//     activeElement = cardsListElement.querySelector(`.selected`);
-//     activeElement.ondragstart = function() {
-//         return false;
-//     };
-// });
-//
-// fieldListElement.addEventListener(`dragend`, (evt) => {
-//     evt.target.classList.remove(`selected`);
-// });
-//
-// cardsListElement.addEventListener(`dragend`, (evt) => {
-//     evt.target.classList.remove(`selected`);
-// });
-//
-// fieldListElement.addEventListener(`dragover`, (evt) => {
-//     evt.preventDefault();
-//
-//     console.log('active', activeElement);
-//     const currentElement = evt.target;
-//     console.log('current', currentElement);
-//
-//     const isMoveable = activeElement !== currentElement &&
-//         currentElement.classList.contains(`field__empty`);
-//
-//     if (!isMoveable) {
-//         console.log('return');
-//         return;
-//     }
-//
-//     const nextElement = currentElement.nextElementSibling;
-//     if (nextElement) {
-//         console.log('nextElemenent', nextElement);
-//     }
-//
-//     if (nextElement !== null && activeElement !== null) {
-//         fieldListElement.insertBefore(activeElement, nextElement);
-//         fieldListElement.prepend(fieldEmpty.cloneNode(true));
-//         fieldListElement.appendChild(fieldEmpty.cloneNode(true));
-//         fieldListElement.removeChild(currentElement);
-//         manaElement.textContent = tempManaAfterDrag + "/10";
-//         mana = tempManaAfterDrag;
-//         manabarFilling();
-//     } else if (activeElement !== null) {
-//         const nextSibling = activeElement.nextElementSibling;
-//         fieldListElement.insertBefore(activeElement, nextSibling);
-//         fieldListElement.prepend(fieldEmpty.cloneNode(true));
-//         fieldListElement.appendChild(fieldEmpty.cloneNode(true));
-//         fieldListElement.removeChild(currentElement);
-//         manaElement.textContent = tempManaAfterDrag + "/10";
-//         mana = tempManaAfterDrag;
-//         manabarFilling();
-//     }
-// });
+                    card.style.position = 'absolute';
+                    moveAt(e);
 
+                    card.style.zIndex = 1000;
 
+                    function moveAt(e) {
+                        card.style.left = e.pageX - shiftX + 'px';
+                        card.style.top = e.pageY - shiftY + 'px';
+                        top = e.pageY - shiftY;
+                    }
+
+                    document.onmousemove = function (e) {
+                        moveAt(e);
+                    };
+
+                    card.onmouseup = function () {
+                        document.onmousemove = null;
+                        card.onmouseup = null;
+
+                        if (parseInt(card.style.top) < handLimits) {
+                            let fieldEmpty = field.querySelector('.field__empty');
+                            if (fieldEmpty) {
+                                field.removeChild(fieldEmpty);
+                            }
+                            field.appendChild(card);
+                            card.style.position = 'static';
+                            card.classList.remove('cards__card');
+                            card.classList.add('field__card');
+
+                            mana = mana - manaSelectedCard;
+                            manabarFilling();
+                        } else {
+                            hand.appendChild(card);
+                            card.style.position = 'static';
+                        }
+                    }
+                }
+                ;
+            }
+        };
+
+        card.ondragstart = function () {
+            return false;
+        };
+    })(cards[i]);
+}
 
 const endTurnButton = document.getElementById('endturn');
 
